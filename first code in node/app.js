d967420f33ed74ae992646f6129d1b5b1d3b9278
dula2000 <96098056+dula2000@users.noreply.github.com>
2023-01-08 23:00:06 +0530

    Path Module

--- first code in node/app.js
+++ first code in node/app.js
@@ -11,4 +11,13 @@
 // var message ='';
 // console.log(global.message);
 
-console.log(module);
+// console.log(module);
+
+// var logger=require('./logger');
+
+// logger.log('message');
+
+const path = require('path');
+
+var pathObj = path.parse(__filename);
+console.log(pathObj);
